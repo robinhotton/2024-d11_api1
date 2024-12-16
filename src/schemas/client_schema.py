6b198cd817133499f70f298f553691e73c3ee115
Robin HOTTON <robin.hotton@hotmail.com>
2024-12-16 17:36:48 +0100

    prettify

--- src/schemas/client_schema.py
+++ src/schemas/client_schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
 class ClientSchema(BaseModel):
     prenomcli:Optional[str] = None
     nomcli:str
--- src/services/client_service.py
+++ src/services/client_service.py
@@ -1,11 +1,14 @@
 from src.repositories.client_repository import get_all_clients, get_client_by_id, create_client
 from src.schemas.client_schema import ClientSchema
 
+
 def get_all(db):
     return get_all_clients(db)
 
+
 def get_by_id(db, id):
     return get_client_by_id(db, id)
+
 
 def create(db, donnees_client: ClientSchema):
     donnees_client = donnees_client.model_dump()
